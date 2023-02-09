c8cdf3dee040b075ceb17719b8fe20cb17c0db2e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-02-09 20:42:40 +0000

    Initial folder structure.

---
+++ pd-janitor/cleaning.py
@@ -0,0 +1,13 @@
+"""
+DataFrame Cleaning
+==================
+
+The script that defines the class the deals with cleaning the dataframe.
+
+"""
+
+import pandas as pd
+
+
+class DataSource:
+    pass
