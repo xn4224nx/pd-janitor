aefe490953b669c098595dc3359c2dbecfa001b2
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-02-10 00:02:16 +0000

    Added some method prototypes.

--- pd-janitor/cleaning.py
+++ pd-janitor/cleaning.py
@@ -10,4 +10,27 @@ import pandas as pd
 
 
 class DataSource:
-    pass
+
+    def __init__(self):
+        pass
+
+    def clean_names(self):
+        pass
+
+    def del_columns(self):
+        pass
+
+    def keep_columns(self):
+        pass
+
+    def del_row_with_null(self):
+        pass
+
+    def del_empty(self):
+        pass
+
+    def del_col_with_null(self):
+        pass
+
+    def format_as_datetime(self):
+        pass
